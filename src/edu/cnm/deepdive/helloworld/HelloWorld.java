c10180df958994560dc2f379e0abbfa96157c269
“JoDeLeon95” <JoDeLeon95@gmail.com>
2019-01-11 14:52:22 -0700

    New project files, with file implementing "Hello World" program.

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * Simple class that displays the 'Hello World message.
+ */
+public class HelloWorld {
+
+	/**
+	 * Prints "Hello World" to standard output.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	
+	}
+
+}
